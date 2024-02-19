fd342ec28781b96f0fa080e998d57c8a49103ff4
Your Name <alexissanmiguel03@gmail.com>
2024-02-18 19:41:29 -0600

    Refactor Turing component and update examples

--- src/Components/Turing.tsx
+++ src/Components/Turing.tsx
@@ -1,37 +1,65 @@
-import { Divider, FormLabel, Heading, Input, Text } from '@chakra-ui/react';
-import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
 import { examples } from '../constants/examples';
 
-const machine = examples[2];
+const machine = examples[1];
 
 const Turing = () => {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
+  const [tape, setTape] = useState<string[]>([]);
+  const [head, setHead] = useState(0);
+  const isRunning = useRef(false);
 
-  function runTuringMachine(input: string) {
-    setInput(input);
-    let state = machine.initialState;
-    const tape = [...input];
-    let position = 0;
+  useEffect(() => {
+    if (isRunning.current) {
+      const timer = setTimeout(runTuringMachine, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [tape, head]);
 
-    while (state.id !== machine.finalState.id) {
-      const symbol = tape[position] || machine.blank;
-      const transition = machine.transitions.find(
-        (t) => t.from.id === state.id && t.read === symbol
-      );
-
-      if (!transition) {
-        throw new Error(
-          `No transition found from state "${state.name}" on symbol "${symbol}"`
-        );
-      }
-
-      tape[position] = transition.write;
-      position += transition.direction === 'R' ? 1 : -1;
-      state = transition.to;
+  function runTuringMachine() {
+    if (!isRunning.current) {
+      // Initialize
+      isRunning.current = true;
+      setTape([...input].concat(machine.blank));
+      setHead(0);
+      return;
     }
 
-    setOutput(tape.join(''));
+    let state = machine.initialState;
+    const symbol = tape[head] || machine.blank;
+    const transition = machine.transitions.find(
+      (t) => t.from.id === state.id && t.read === symbol
+    );
+
+    if (!transition) {
+      throw new Error(
+        `No transition found from state "${state.name}" on symbol "${symbol}"`
+      );
+    }
+
+    // Update tape and head
+    const newTape = [...tape];
+    newTape[head] = transition.write;
+    setTape(newTape);
+    setHead(head + (transition.direction === 'R' ? 1 : -1));
+    state = transition.to;
+
+    if (state.id === machine.finalState.id) {
+      // Stop when reaching final state
+      isRunning.current = false;
+      setOutput(newTape.join(''));
+    }
   }
 
   return (
@@ -43,25 +71,38 @@ const Turing = () => {
       <form>
         <FormLabel>Entrada</FormLabel>
 
-        <Input value={input} onChange={(e) => runTuringMachine(e.target.value)} />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+
+        <Button
+          type='button'
+          mt={4}
+          onClick={() => {
+            runTuringMachine();
+          }}
+        >
+          Ejecutar
+        </Button>
 
         <Divider my={4} />
-        {output && (
-          <>
-            <Heading size='md' mt={4}>
-              Entrada
-            </Heading>
-            <Text mt={1} fontSize={'xl'}>
-              {input}
-            </Text>
-            <Heading size='md' mt={4}>
-              Salida
-            </Heading>
-            <Text mt={1} fontSize={'xl'}>
-              {output}
-            </Text>
-          </>
-        )}
+
+        <Text mt={1} fontSize={'xl'}>
+          <Flex gap={3}>
+            {tape.map((symbol, index) => (
+              <Flex key={index} direction={'column'} alignItems={'center'} gap={1}>
+                <Box
+                  key={index}
+                  style={{ color: index === head ? '#2b6cb0' : 'white' }}
+                  px={3}
+                  border={'2px solid white'}
+                  borderRadius={'md'}
+                >
+                  <Text fontSize={'3xl'}>{symbol}</Text>
+                </Box>
+                {index === head && <Text color={'#2b6cb0'}>▲</Text>}
+              </Flex>
+            ))}
+          </Flex>
+        </Text>
       </form>
     </>
   );
--- src/constants/examples.ts
+++ src/constants/examples.ts
@@ -1,29 +1,3 @@
-/* 
-interface State {
-  name: string;
-  id: number;
-}
-
-interface TuringMachine {
-  description: string;
-  states: State[];
-  alphabet: string[];
-  blank: string;
-  finalState: State;
-  transitions: Transition[];
-  initialState: State;
-}
-
-interface Transition {
-  id: number;
-  from: State;
-  to: State;
-  read: string;
-  write: string;
-  direction: string;
-}
-
-*/
 const blank = '△';
 
 export const examples: TuringMachine[] = [
