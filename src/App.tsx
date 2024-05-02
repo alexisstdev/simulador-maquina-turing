473fd1c087ba52c982fda76ca1a984a8333129f4
Your Name <alexissanmiguel03@gmail.com>
2024-05-02 11:19:52 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
 import { Container, Divider, Heading, Text } from '@chakra-ui/react';
 import { AnimatePresence } from 'framer-motion';
-import Turing from './Components/Turing';
-import useMaquinas from './hooks/useMachines';
 import { Route, Routes } from 'react-router';
-import Home from './Components/Home';
-import MaquinaProvider from './Components/MaquinaProvider';
 import { Link } from 'react-router-dom';
+import Home from './Components/Home';
 
 function App() {
-  useMaquinas();
-
   return (
     <AnimatePresence mode='wait'>
-      <MaquinaProvider>
-        <Container maxW={'7xl'} py={8}>
-          <Heading as={'h1'} size={'xl'} textAlign={'center'} fontFamily={'monospace'}>
-            <Link to={'/'}>Simulador Máquina de Turing</Link>
-          </Heading>
-          <Text mt={4} fontWeight={'bold'} fontSize={'lg'}>
-            Creado por: {'  '}
-          </Text>
-          <Text>David Perez Compean - 21100266</Text>
-          <Text>Esdras Daniel Amaya Vela - 21100155</Text>
-          <Text>Alexis Sanmiguiel Torres - 21100288</Text>
+      <Container maxW={'7xl'} py={8}>
+        <Heading as={'h1'} size={'xl'} textAlign={'center'} fontFamily={'monospace'}>
+          <Link to={'/'}>Lenguaje de programación NOVA</Link>
+        </Heading>
+        <Text mt={4} fontWeight={'bold'} fontSize={'lg'}>
+          Creado por:
+        </Text>
+        <Text>David Perez Compean - 21100266</Text>
+        <Text>Esdras Daniel Amaya Vela - 21100155</Text>
+        <Text>Alexis Sanmiguel Torres - 21100288</Text>
 
-          <Divider mt={4} mb={10} />
+        <Divider mt={4} mb={4} />
 
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/crear' element={<Turing />} />
-          </Routes>
-        </Container>
-      </MaquinaProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/diagrama' element={<></>} />
+        </Routes>
+      </Container>
     </AnimatePresence>
   );
 }
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,134 +1,150 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  FormLabel,
-  Input,
-  Select,
-  Text,
-} from '@chakra-ui/react';
-import { FormEvent, useContext, useEffect } from 'react';
-import { FiEdit3 } from 'react-icons/fi';
-import { useNavigate } from 'react-router';
-import useMaquinas from '../hooks/useMachines';
-import useTuring from '../hooks/useTuring';
-import { MaquinaTuring } from '../types/types';
-import Cinta from './Cinta';
-import Detalles from './Detalles';
-import Diagrama from './Diagrama';
-import { Context } from './MaquinaProvider';
+import { Box, Button, Flex, IconButton, Input, Text } from '@chakra-ui/react';
+import { FormEvent } from 'react';
+import { FiUpload } from 'react-icons/fi';
+
+const exampleTokens = `[
+  ('case', 'CASE'),
+  (' ', 'WHITESPACE'),
+  ('x', 'IDENTIFIER'),
+  (' ', 'WHITESPACE'),
+  ('=', 'ASSIGNMENT'),
+  (' ', 'WHITESPACE'),
+  ('5', 'NUMBER'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('switch', 'SWITCH'),
+  (' ', 'WHITESPACE'),
+  ('x', 'IDENTIFIER'),
+  (' ', 'WHITESPACE'),
+  ('{', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('case', 'CASE'),
+  (' ', 'WHITESPACE'),
+  ('5', 'NUMBER'),
+  (':', 'COLON'),
+  (' ', 'WHITESPACE'),
+  ('break', 'BREAK'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('default', 'IDENTIFIER'),
+  (':', 'COLON'),
+  (' ', 'WHITESPACE'),
+  ('break', 'BREAK'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('}', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('catch', 'CATCH'),
+  (' ', 'WHITESPACE'),
+  ('(', 'PARENTHESIS'),
+  ('e', 'IDENTIFIER'),
+  (')', 'PARENTHESIS'),
+  (' ', 'WHITESPACE'),
+  ('{', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('if', 'IF'),
+  (' ', 'WHITESPACE'),
+  ('(', 'PARENTHESIS'),
+  ('e', 'IDENTIFIER'),
+  (')', 'PARENTHESIS'),
+  (' ', 'WHITESPACE'),
+  ('{', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('for', 'FOR'),
+  (' ', 'WHITESPACE'),
+  ('(', 'PARENTHESIS'),
+  ('let', 'LET'),
+  (' ', 'WHITESPACE'),
+  ('i', 'IDENTIFIER'),
+  (' ', 'WHITESPACE'),
+  ('=', 'ASSIGNMENT'),
+  (' ', 'WHITESPACE'),
+  ('0', 'NUMBER'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('i', 'IDENTIFIER'),
+  (' ', 'WHITESPACE'),
+  ('<', 'LESS_THAN'),
+  (' ', 'WHITESPACE'),
+  ('5', 'NUMBER'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('i', 'IDENTIFIER'),
+  ('+', 'PLUS'),
+  ('+', 'PLUS'),
+  (')', 'PARENTHESIS'),
+  (' ', 'WHITESPACE'),
+  ('{', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('continue', 'CONTINUE'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('}', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('}', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('else', 'ELSE'),
+  (' ', 'WHITESPACE'),
+  ('{', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('return', 'RETURN'),
+  (';', 'SEMICOLON'),
+  (' ', 'WHITESPACE'),
+  ('}', 'BRACES'),
+  (' ', 'WHITESPACE'),
+  ('}', 'BRACES')
+`;
 
 export default function Home() {
-  const {
-    entrada,
-    cinta,
-    cabeza,
-    isEjecutando,
-    setEntrada,
-    ejecutarMaquina,
-    setCinta,
-    setCabeza,
-  } = useTuring();
-
-  const { machines } = useMaquinas();
-  const { setMaquina, maquina: selectedMaquina } = useContext(Context);
-  const navigate = useNavigate();
-
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-
-    ejecutarMaquina();
   }
-
-  useEffect(() => {
-    if (machines.length > 0) {
-      setMaquina(machines[4]);
-    }
-  }, [machines, setMaquina]);
-
-  useEffect(() => {
-    if (selectedMaquina) {
-      setCinta([...entrada].concat(Array(60).fill(selectedMaquina.blanco)));
-    }
-  }, [selectedMaquina, setCinta]);
 
   return (
     <>
-      <Flex
-        justifyContent={'space-between'}
-        alignItems={'flex-start'}
-        mt={8}
-        mb={4}
-        gap={8}
-      >
+      <Flex justifyContent={'space-between'} alignItems={'flex-start'} gap={8}>
         <Box flex={0.5}>
-          <Flex justifyContent={'space-between'} alignItems={'center'}>
-            <Text fontSize={'2xl'}>Cargar máquina</Text>
-            <Button
-              leftIcon={<FiEdit3 />}
-              onClick={() => {
-                navigate('/crear');
-              }}
-              ml={4}
-            >
-              Editar máquina
-            </Button>
-          </Flex>
-          <Select
-            placeholder='Seleccionar máquina'
-            mt={4}
-            value={selectedMaquina.descripcion}
-            onChange={(e) => {
-              const maquinaSeleccionada = machines.find(
-                (machine) => machine.descripcion === e.target.value
-              ) as MaquinaTuring;
-
-              if (!maquinaSeleccionada) return;
-
-              return setMaquina(maquinaSeleccionada);
-            }}
-          >
-            {machines.map((machine, index) => (
-              <option key={index} value={machine.descripcion} style={{ color: 'black' }}>
-                {machine.descripcion}
-              </option>
-            ))}
-          </Select>
-
           <form onSubmit={handleSubmit}>
-            <FormLabel mt={4}>Entrada</FormLabel>
-
+            <Flex justifyContent={'space-between'} alignItems={'center'}>
+              <Text>Código</Text>
+              <IconButton
+                type='submit'
+                colorScheme='gray'
+                aria-label='Cargar archivo'
+                icon={<FiUpload />}
+              />
+            </Flex>
             <Input
-              value={entrada}
-              onChange={(e) => {
-                setEntrada(e.target.value);
-
-                setCinta(
-                  [...e.target.value].concat(Array(60).fill(selectedMaquina.blanco))
-                );
-              }}
+              px={4}
+              p={2}
+              mt={2}
+              h={64}
+              placeholder='Escribe tu código aquí...'
+              rows={20}
+              as={'textarea'}
             />
-            <Button type='submit' mt={4} colorScheme='blue' size={'lg'} width={'100%'}>
-              Ejecutar
-            </Button>
+            <Flex gap={4}>
+              <Button type='submit' mt={4} colorScheme='blue' width={'100%'}>
+                Guardar archivo
+              </Button>
+            </Flex>
           </form>
         </Box>
         <Box flex={0.5}>
-          <Diagrama />
+          <Text>Código</Text>
+          <Box
+            h={64}
+            borderWidth={1}
+            borderRadius={8}
+            overflow={'auto'}
+            px={4}
+            p={2}
+            mt={6}
+          >
+            <Text>{exampleTokens}</Text>
+          </Box>
         </Box>
       </Flex>
-      <Divider my={8} />
-      <Detalles />
-      <Cinta
-        cinta={cinta}
-        cabeza={cabeza}
-        setCabeza={setCabeza}
-        isEjecutando={isEjecutando}
-      />
-      <Text opacity={0.7} textAlign='center'>
-        Seleccione una celda de la cinta para posicionar el cabezal.
-      </Text>
     </>
   );
 }
